16a61f0fd6f3c1cc0652ad3742c8dfacd3a26b37
RITUNJ KAUSHIK <admin@Ritunj-Kaushik.local>
2026-01-31 21:42:17 +0530

    Fix CORS for deployed frontend

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors({
   origin: [
       "http://localhost:5173",
-      "https://property-management.vercel.app"
+      "https://property-management-backend-six.vercel.app"
     ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
